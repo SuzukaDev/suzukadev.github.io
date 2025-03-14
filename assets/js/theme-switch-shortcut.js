240b466a3c62267224353c3ec856511d884c4154
SuzukaDev <orbernabeu@gmail.com>
2025-03-14 20:49:24 +0100

    custom main.js and theme-switch-shortcut.js

---
+++ assets/js/theme-switch-shortcut.js
@@ -0,0 +1,24 @@
+// When pressing ALT+L clicks the theme-toggle button
+
+document.addEventListener('keydown', function(event) {
+	// if (event.key == 'z' && event.ctrlKey) {
+	// if (event.key == 'l' && event.ctrlKey && event.shiftKey) {
+	// if (event.key == 'o' && event.ctrlKey && event.shiftKey) {
+	// if (event.key == 'l' && event.ctrlKey) {
+	// if (event.key == 'l' && event.altKey) {
+	// if (event.key == 'l' && event.altKey) {
+	// if (event.key == 'z') {
+
+	if (event.key == 'l' && event.altKey) {
+
+		// alert("weeee assetssss");
+        // CTRL + Z trigger
+		// themeSwitch.click()
+		const themeSwitch = document.getElementById('theme-toggle')
+
+		themeSwitch.click()
+    }
+});
+
+
+// alert("venga hombre");
