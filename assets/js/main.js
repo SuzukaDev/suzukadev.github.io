9068e88e9bc6075fda27289862e515d720a78e72
SuzukaDev <orbernabeu@gmail.com>
2025-05-11 01:56:49 +0200

    not using js no-loading-transitions (at the moment)

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,6 @@ import "./gallery.js";
 import "./lazysizes.js";
 import "./lightbox.js";
 import "./custom.js";
-import "./no-loading-transitions.js";
+// import "./no-loading-transitions.js";
 import "./theme-switch-shortcut.js";
 import "./scroll-to-top-button.js";
